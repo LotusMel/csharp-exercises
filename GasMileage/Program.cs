39e6bb86a0b083aa98711d2c96a77b4273b0e59b
Melva Lang <mjlang74@gmail.com>
2020-01-08 12:03:16 -0600

    Added GasMileage

---
+++ GasMileage/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace GasMileage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many miles have you driven?: ");
+           
+
+            string mileage = Console.ReadLine();
+            float miles = float.Parse(mileage);
+
+
+            Console.Write("How many gallons of gas have you consumed? ");
+            
+
+            string gas = Console.ReadLine();
+            float gallons = float.Parse(gas);
+
+            
+
+            float mpg = miles / gallons;
+
+            Console.WriteLine("Your miles per gallon is: " + mpg);
+            Console.ReadLine();
+        }
+    }
+}
