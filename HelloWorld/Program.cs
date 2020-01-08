dddd33e8016a88b85c085e46b5d4dc8aaeb830ba
Melva Lang <mjlang74@gmail.com>
2020-01-07 23:57:00 -0600

    Added Greeting method

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,9 +4,19 @@ namespace HelloWorld
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            
+            Greeting();
+            
+        }
+
+        public static void Greeting()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
             Console.ReadLine();
         }
     }
