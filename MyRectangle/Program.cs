4850beeaec8e0f9e314c895ec21b2c833d128ffd
Melva Lang <mjlang74@gmail.com>
2020-01-08 00:59:19 -0600

    Rectangle changes

---
+++ MyRectangle/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MyRectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello! I'm here to help you find the area of a rectangle. " +
+                "Press 'Enter' to begin.");
+            Console.ReadLine();
+
+            Rectangle();
+        }
+
+        public static void Rectangle()
+        {
+            Console.Write("Enter the length of your rectangle: ");
+            float length = float.Parse(Console.ReadLine());
+
+            Console.Write("Enter the height of the rectangle: ");
+            float height = float.Parse(Console.ReadLine());
+
+            float area = length * height;
+
+            Console.WriteLine("The area of the rectangle is: " + area);
+            Console.ReadLine();
+        }
+    }
+}
