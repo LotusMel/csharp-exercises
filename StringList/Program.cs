0e9f4979e47f5e8e7be62b01ea951e09ea546727
Melva Lang <mjlang74@gmail.com>
2020-01-14 12:26:54 -0600

    Class 2 Prep: StringList

---
+++ StringList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWords = new List<string>() { "Melie", "soda", "vernacular", "spaghetti", "write" };
+            PentaScribe(myWords);
+        }
+
+        public static void PentaScribe(List<string> words)
+        {
+
+            foreach (string word in words)
+            {
+                if (word.Length == 5)
+                    Console.Write(word + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
