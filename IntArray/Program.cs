061e6faf44568082d748eea1bc931cd70c81e316
Melva Lang <mjlang74@gmail.com>
2020-01-14 19:04:28 -0600

    Added Int array

---
+++ IntArray/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace IntArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 1, 2, 3, 5, 8 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
