a7c3bab3591f76e3cb8cdbec2c25002df057a633
Melva Lang <mjlang74@gmail.com>
2020-04-08 23:31:36 -0500

    Student class for School

---
+++ Restaurant/MenuItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    class MenuItem
+    {
+        public enum Category
+        {
+            Appetizer, Beverages, Burgers, Desserts, Pizzas, Sides
+        }
+
+        //public bool IsNew
+        //{
+        //    get
+        //    {
+        //        return AddedOn.Date >= DateTime.Now.Date.AddMonths(-3);
+        //    }
+        //}
+    }
+}
---
+++ School/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student girl1 = new Student();
+            girl1.Name = "Emma";
+            Console.WriteLine(girl1.Name);
+            Console.ReadLine();
+        }
+    }
+
+    public class Student
+    {
+        //private string name;
+        //private int studentid;
+        //private int numberofcredits = 0;
+        //private double gpa = 0;
+
+        public string Name { get; set; }
+        public int StudentId { get; set; }
+        public int NumberOfCredits { get; set; }
+        public double Gpa { get; set; }
+
+    }
+}
